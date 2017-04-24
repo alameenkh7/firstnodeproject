567560a24148ee4df0af11cc1e502b712401bc48
al ameen <alameenkh7@gmail.com>
2017-04-24 12:05:13 +0000

    test

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ app.get('/home',function(req,res){
     res.send("hello world");
 })
 
-app.listen('process.env.PORT || <default port>',function(){
+app.listen('8080',function(){
     console.log("app is listening to 3000");
 })
 
