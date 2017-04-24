a2c3475c30c1684786a8c25732e045fe4e319848
al ameen <alameenkh7@gmail.com>
2017-04-24 08:13:39 +0000

    test

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 var express=require("express");
 var app=express();
 
-app.get('/',function(req,res){
-    res.end("hello world");
+app.get('/home',function(req,res){
+    res.send("hello world");
 })
 
-app.listen('process.env.PORT || 80',function(){
-    console.log("app is listening to 80");
+app.listen('process.env.PORT || 3000',function(){
+    console.log("app is listening to 3000");
 })
 
 
